fa25a23cd7eef4e4fe9d8047e8fb53d74377586d
Scott Susanto <scottsus@usc.edu>
2022-06-09 03:56:03 -0700

    Refactor ListByRepositoryQuery

--- internal/repos/bitbucketserver_test.go
+++ internal/repos/bitbucketserver_test.go
@@ -3,6 +3,7 @@ package repos
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -264,7 +265,7 @@ func TestBitbucketServerSource_ListByReposOnly(t *testing.T) {
 	}
 }
 
-func TestBitbucketServerSource_ListByRepositoryQueryDefault(t *testing.T) {
+func TestBitbucketServerSource_ListByRepositoryQuery(t *testing.T) {
 	repos := GetReposFromTestdata(t)
 
 	type Results struct {
@@ -310,169 +311,58 @@ func TestBitbucketServerSource_ListByRepositoryQueryDefault(t *testing.T) {
 	defer server.Close()
 
 	cases, svc := GetConfig(t, server)
-	for name, config := range cases {
-		t.Run(name, func(t *testing.T) {
-			s, err := newBitbucketServerSource(&svc, config, nil)
-			if err != nil {
-				t.Fatal(err)
-			}
 
-			s.config.RepositoryQuery = []string{
+	tcs := []struct {
+		queries []string
+		exp     int
+	}{
+		{
+			[]string{
 				"?projectName=go-langserver",
 				"?projectName=python-langserver",
 				"?projectName=python-langserver-fork",
 				"?projectName=rgp",
 				"?projectName=rgp-unavailable",
-			}
-
-			ctxWithTimeout, cancelFunction := context.WithTimeout(context.Background(), 5*time.Second)
-			defer cancelFunction()
-
-			results := make(chan SourceResult, 20)
-			defer close(results)
-
-			s.ListRepos(ctxWithTimeout, results)
-			VerifyData(t, ctxWithTimeout, 4, results)
-		})
-	}
-}
-
-func TestBitbucketServerSource_ListByRepositoryQueryAll(t *testing.T) {
-	repos := GetReposFromTestdata(t)
-
-	type Results struct {
-		*bitbucketserver.PageToken
-		Values any `json:"values"`
-	}
-
-	pageToken := bitbucketserver.PageToken{
-		Size:          1,
-		Limit:         1000,
-		IsLastPage:    true,
-		Start:         1,
-		NextPageStart: 1,
-	}
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/rest/api/1.0/repos", func(w http.ResponseWriter, r *http.Request) {
-		projectName := r.URL.Query().Get("projectName")
-
-		if projectName == "" {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(Results{
-				PageToken: &pageToken,
-				Values:    repos,
-			})
-		} else {
-			for _, repo := range repos {
-				if projectName == repo.Name {
-					w.Header().Set("Content-Type", "application/json")
-					w.WriteHeader(http.StatusOK)
-					json.NewEncoder(w).Encode(Results{
-						PageToken: &pageToken,
-						Values:    []bitbucketserver.Repo{repo},
-					})
-				}
-			}
-		}
-	})
-
-	server := httptest.NewServer(mux)
-	defer server.Close()
-
-	cases, svc := GetConfig(t, server)
-	for name, config := range cases {
-		t.Run(name, func(t *testing.T) {
-			s, err := newBitbucketServerSource(&svc, config, nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			s.config.RepositoryQuery = []string{
+			},
+			4,
+		},
+		{
+			[]string{
 				"all",
-			}
-
-			ctxWithTimeout, cancelFunction := context.WithTimeout(context.Background(), 5*time.Second)
-			defer cancelFunction()
-
-			results := make(chan SourceResult, 20)
-			defer close(results)
-
-			s.ListRepos(ctxWithTimeout, results)
-			VerifyData(t, ctxWithTimeout, 4, results)
-		})
-	}
-}
-
-func TestBitbucketServerSource_ListByRepositoryQueryNone(t *testing.T) {
-	repos := GetReposFromTestdata(t)
-
-	type Results struct {
-		*bitbucketserver.PageToken
-		Values any `json:"values"`
-	}
-
-	pageToken := bitbucketserver.PageToken{
-		Size:          1,
-		Limit:         1000,
-		IsLastPage:    true,
-		Start:         1,
-		NextPageStart: 1,
-	}
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/rest/api/1.0/repos", func(w http.ResponseWriter, r *http.Request) {
-		projectName := r.URL.Query().Get("projectName")
-
-		if projectName == "" {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(Results{
-				PageToken: &pageToken,
-				Values:    repos,
-			})
-		} else {
-			for _, repo := range repos {
-				if projectName == repo.Name {
-					w.Header().Set("Content-Type", "application/json")
-					w.WriteHeader(http.StatusOK)
-					json.NewEncoder(w).Encode(Results{
-						PageToken: &pageToken,
-						Values:    []bitbucketserver.Repo{repo},
-					})
-				}
-			}
-		}
-	})
-
-	server := httptest.NewServer(mux)
-	defer server.Close()
-
-	cases, svc := GetConfig(t, server)
-	for name, config := range cases {
-		t.Run(name, func(t *testing.T) {
-			s, err := newBitbucketServerSource(&svc, config, nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			s.config.RepositoryQuery = []string{
+			},
+			4,
+		},
+		{
+			[]string{
 				"none",
-			}
-
-			ctxWithTimeout, cancelFunction := context.WithTimeout(context.Background(), 5*time.Second)
-			defer cancelFunction()
-
-			results := make(chan SourceResult, 20)
-			defer close(results)
-
-			s.ListRepos(ctxWithTimeout, results)
-			VerifyData(t, ctxWithTimeout, 0, results)
-		})
+			},
+			0,
+		},
 	}
+
+	for _, tc := range tcs {
+		tc := tc
+		for name, config := range cases {
+			t.Run(name, func(t *testing.T) {
+				s, err := newBitbucketServerSource(&svc, config, nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				s.config.RepositoryQuery = tc.queries
+
+				ctxWithTimeout, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+				defer cancel()
+
+				results := make(chan SourceResult, 10)
+				defer close(results)
+
+				s.ListRepos(ctxWithTimeout, results)
+				VerifyData(t, ctxWithTimeout, tc.exp, results)
+			})
+		}
+	}
+
 }
 
 func TestBitbucketServerSource_ListByProjectKey(t *testing.T) {
@@ -509,7 +399,6 @@ func TestBitbucketServerSource_ListByProjectKey(t *testing.T) {
 			PageToken: &pageToken,
 			Values:    values,
 		})
-
 	})
 
 	server := httptest.NewServer(mux)
@@ -589,6 +478,7 @@ func VerifyData(t *testing.T, ctx context.Context, numExpectedResults int, resul
 	numReceivedFromResults := 0
 
 	if numTotalResults != numExpectedResults {
+		fmt.Println("numTotalResults:", numTotalResults, ", numExpectedResults:", numExpectedResults)
 		t.Fatal(errors.New("wrong number of results"))
 	}
 
